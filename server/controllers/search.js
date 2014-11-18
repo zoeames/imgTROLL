e5480d54595f3f42a38f79655ee49e60543405a5
Dave Boling <kadowki@gmail.com>
2014-11-18 13:17:51 -0600

    Added basic request and cheerio to package.json

---
+++ server/controllers/search.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var requestWebsite = require('request'),
+    cheerio        = require('cheerio');
+
+exports.crawl = {
+  handler: function(request, reply){
+    requestWebsite('http://www.cnn.com', function(error, response, body){
+      if (!error && response.statusCode === 200){
+        reply(body);
+      }
+    });
+  }
+};
+
+
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,12 @@
 'use strict';
 
 //import controller here
-var home = require('../controllers/home');
+var home   = require('../controllers/home'),
+    search = require('../controllers/search');
 
 module.exports = [
   {method: 'GET',    path: '/',           config: home.index},
   {method: 'GET',    path: '/about',      config: home.about},
+  {method: 'GET',    path: '/search',     config: search.crawl},
   {method: 'GET',    path: '/{params*}',  handler: {directory: {path: 'public'}}}
 ];
