500a9e55fa80b7e3585ca98e6ef77536cafe9aa7
Matt Keef <mkeef1@gmail.com>
2014-11-19 12:13:21 -0600

    Message send, showAll and showOne added

---
+++ server/models/message.js
@@ -0,0 +1,14 @@
+/**
+ * Created by matt on 11/19/14.
+ */
+
+var mongoose = require('mongoose'),
+    User     = require('./user'),
+    Schema   = mongoose.Schema,
+    MessageSchema = new mongoose.Schema(
+        {body: String, sender: {type: Schema.Types.ObjectId, ref: 'User'}, receiver: {type: Schema.Types.ObjectId, ref: 'User'}}),
+    Message = mongoose.model('Message', MessageSchema);
+
+
+
+module.exports = Message;
