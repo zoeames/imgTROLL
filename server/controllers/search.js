fe3d47408a2424d93306e6e8b5d5908cbd064141
Dave Boling <kadowki@gmail.com>
2014-11-18 22:43:00 -0600

    Added search model and modularized code slightly

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,42 +1,14 @@
 'use strict';
 
-var requestWebsite = require('request'),
-    cheerio        = require('cheerio'),
-    _              = require('underscore');
+
+var Search = require('../models/search');
 
 exports.crawl = {
   handler: function(request, reply){
-    requestWebsite('http://www.ocharleys.com/', function(error, response, body){
-      if (!error && response.statusCode === 200){
-
-        var $ = cheerio.load(body),
-        anchorTags = $('a'),
-        keys = Object.keys(anchorTags),
-        a = keys.map(function(k){
-          return checkRoute(anchorTags[k].attribs);
-        });
-
-        a = _.compact(a);
-        a = _.uniq(a);
-
-        reply({a: a});
-      }
+    Search.getLinks('http://www.ocharleys.com', function(links){
+      reply(links);
     });
   }
 };
-
-function checkRoute(link){
-  var re = new RegExp(/^\/[a-zA-Z0-9]*/);
-
-  //check undefined
-  if(link === undefined){ return; }
-
-  if(re.test(link.href)){
-    //console.log(link.href.match(re)[0]);
-    return link.href.match(re)[0];
-  }else{
-    return;
-  }
-}
 
 
--- server/models/search.js
+++ server/models/search.js
@@ -1,3 +1,47 @@
-'use strict'
+'use strict';
 
-var mongoose = require('mongoose');
+var mongoose       = require('mongoose'),
+    requestWebsite = require('request'),
+    cheerio        = require('cheerio'),
+    _              = require('underscore'),
+    schema = new mongoose.Schema({name: String, mainUrl: String, images: [String]}),
+    Search = mongoose.model('Search', schema);
+
+
+//CLASS METHODS
+Search.getLinks = function(link, cb){
+    requestWebsite(link, function(error, response, body){
+      if (!error && response.statusCode === 200){
+
+        var $ = cheerio.load(body),
+        anchorTags = $('a'),
+        keys = Object.keys(anchorTags),
+        a = keys.map(function(k){
+          return checkRoute(anchorTags[k].attribs);
+        });
+
+        a = _.compact(a);
+        a = _.uniq(a);
+
+        cb(a);
+      }
+    });
+};
+
+//Parse homelinks into http links
+function checkRoute(link){
+  var re = new RegExp(/^\/[a-zA-Z0-9\-]*/);
+
+  //check undefined
+  if(link === undefined){ return; }
+
+  if(re.test(link.href)){
+    //console.log(link.href.match(re)[0]);
+    return link.href.match(re)[0];
+  }else{
+    return;
+  }
+}
+
+module.exports = Search;
+
