4621d89f214d3a521d9b3ae601c2053721704159
Dave Boling <kadowki@gmail.com>
2014-11-19 09:31:39 -0600

    Added regex for testing relative routes

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -5,7 +5,7 @@ var Search = require('../models/search');
 
 exports.crawl = {
   handler: function(request, reply){
-    Search.getLinks('http://www.ocharleys.com', function(links){
+    Search.getLinks('http://www.ocharleys.com/', function(links){
       reply(links);
     });
   }
--- server/models/search.js
+++ server/models/search.js
@@ -9,39 +9,55 @@ var mongoose       = require('mongoose'),
 
 
 //CLASS METHODS
-Search.getLinks = function(link, cb){
-    requestWebsite(link, function(error, response, body){
-      if (!error && response.statusCode === 200){
+Search.getLinks = function(website, cb){
+  website = removeEndingSlash(website);
+  requestWebsite(website, function(error, response, body){
+    if (!error && response.statusCode === 200){
 
-        var $ = cheerio.load(body),
-        anchorTags = $('a'),
-        keys = Object.keys(anchorTags),
-        a = keys.map(function(k){
-          return checkRoute(anchorTags[k].attribs);
-        });
+      var $ = cheerio.load(body),
+      anchorTags = $('a'),
+      keys = Object.keys(anchorTags),
+      a = keys.map(function(k){
+        return checkRoute(anchorTags[k].attribs, website);
+      });
 
-        a = _.compact(a);
-        a = _.uniq(a);
+      a = _.compact(a);
+      a = _.uniq(a);
 
-        cb(a);
-      }
-    });
+      cb(a);
+    }
+  });
 };
 
+
+
+
+module.exports = Search;
+
+
+
+
 //Parse homelinks into http links
-function checkRoute(link){
+function checkRoute(link, root){
   var re = new RegExp(/^\/[a-zA-Z0-9\-]*/);
 
   //check undefined
   if(link === undefined){ return; }
 
   if(re.test(link.href)){
-    //console.log(link.href.match(re)[0]);
-    return link.href.match(re)[0];
+    return root + link.href.match(re)[0];
   }else{
     return;
   }
 }
 
-module.exports = Search;
+function removeEndingSlash(website){
+  //checks for any string that end with a '/' character
+  var removeEnd = new RegExp(/\/$/);
+  if(removeEnd.test(website)){
+    return website.substring(0, website.length-1);
+  }else{
+    return website;
+  }
+}
 
