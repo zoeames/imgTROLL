816186563c9439e1625ea1d7ab29bf5fbb1f3ccd
Dave Boling <kadowki@gmail.com>
2014-11-20 16:15:07 -0600

    Added the beggining of crazy stuff

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -6,7 +6,16 @@ var Search = require('../models/search'),
 
 exports.crawl = {
   handler: function(request, reply){
-    var site= 'http://www.ocharleys.com/';
+    var site = 'http://www.espn.com/';
+
+    //TO BE REMOVED
+/*
+    var search = new Search({name: 'MySearch', mainUrl: site, images: []});
+    search.depthFinder(search.mainUrl, 3, function(){
+      reply('it worked');
+    });
+*/
+
     Search.urlValidate(site, function(err, xyz){
 //      console.log('Err in urlValidate>>>>>', err);
 //      console.log('1 in urlValidate>>>>>', xyz);
@@ -15,7 +24,7 @@ exports.crawl = {
         reply('Error- invalid url');
       }else{
         var search = new Search({name: 'MySearch', mainUrl: site, images: []}),
-        urlsArray  = [site];
+        urlsArray  = [site, 'http://www.ocharleys.com', 'http://www.cnn.com'];
 
         async.forEach(urlsArray, function(url, cb){
           search.scrubImages(url, '000000000000000000000001', function(err){
