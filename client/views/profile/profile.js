dfe0d11a1d68e90150da3c66706afef2d1f0308d
Joy Pratt <joympratt@gmail.com>
2014-11-23 16:42:55 -0600

    fix front-end errors

--- client/views/profile/profile.js
+++ client/views/profile/profile.js
@@ -3,9 +3,8 @@
 
   angular.module('troll')
   .controller('ProCtrl', ['$scope', 'User', '$http', function($scope, User, $http){
-    var user = User.user;
-    $scope.username=user.username;
-    console.log('User.user . . . . ', User.user);
+    $scope.user = User.user;
+
     console.log($scope.username);
 
     $scope.toggleStuff = function(){
