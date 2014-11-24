e976b560fcb22cea8c41d52ba96a1b8513ef809e
Matt Keef <mkeef1@gmail.com>
2014-11-24 09:53:29 -0600

    showmessges done

--- server/routes/definitions/messages/get_allMessages.js
+++ server/routes/definitions/messages/get_allMessages.js
@@ -5,8 +5,9 @@ var Message = require('../../../models/message');
 module.exports = {
 	description: 'all messages',
 	handler: function(request, reply){
-			Message.find(function(err, messages){
-				reply({data:messages});
+      var sender = Message.where({id: request.payload});
+			sender.find(function(err, messages){
+				reply(messages);
 		});
 	}
 };
