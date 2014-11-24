c883cb4680846f2c8ccf8f0f3cd99fc12e22e1bb
Matt Keef <mkeef1@gmail.com>
2014-11-23 18:19:37 -0600

    body saving

--- server/routes/definitions/messages/post_message.js
+++ server/routes/definitions/messages/post_message.js
@@ -6,8 +6,10 @@ var Message = require('../../../models/message');
 module.exports = {
     description: 'Sending message',
     handler: function(req, rep){
-        var message = new Message(req.payload);
-        message.save(function(err, message){
+        var message = new Message({body: req.payload.body});
+        console.log('req.payload', req.payload);
+        message.save(function(){
+        console.log('message', message);
             rep(message);
         });
     }
